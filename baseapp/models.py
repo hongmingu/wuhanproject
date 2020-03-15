3949ec6bc2d9b69232a7bcbaeb14e745c7c006c7
hongmingu <gmh397@nate.com>
2020-03-16 01:52:02 +0900

    digimon adventure

--- baseapp/models.py
+++ baseapp/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from ckeditor.fields import RichTextField
 
 
 # Create your models here.
@@ -90,15 +89,3 @@ class BBCItem(models.Model):
 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-
-
-class Post(models.Model):
-    text = RichTextField(default=None, blank=None, null=True, max_length=2000)
-    nickname = models.CharField(default=None, blank=None, null=True, max_length=255)
-
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return "%s" % self.when
-
